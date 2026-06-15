29e4dae8db3eb44ce19a3265df7e02e958292436
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-15 16:27:36 +0530

    Init: Initialize project with default styles and dependencies

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "react-hot-toast";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<Toaster
+			position="top-right"
+			reverseOrder={false}
+			gutter={8}
+			toastOptions={{
+				style: {
+					padding: "1rem",
+					borderRadius: "10rem",
+				},
+			}}
+		/>
+		<App />
+	</StrictMode>
+);
