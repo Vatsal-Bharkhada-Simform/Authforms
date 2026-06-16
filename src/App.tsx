431a10399b9a4fb13eb6fc877bfa09cb75840c86
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-16 18:51:14 +0530

    Add: Auth layout wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router";
 import { SignUp } from "./pages/SignUp";
 import { Login } from "./pages/Login";
+import { AuthLayout } from "./layout/AuthLayout";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/auth">
+				<Route path="/auth" element={<AuthLayout />}>
 					<Route path="/auth/signup" element={<SignUp />} />
 					<Route path="/auth/login" element={<Login />} />
 				</Route>
