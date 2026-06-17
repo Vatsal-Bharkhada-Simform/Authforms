fcffe1d1bcdbf7d21a9d3dc27355a43e52d1cb18
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-17 17:15:34 +0530

    Feat: Add homepage component. Add protected route component to prevent unauthorized homepage access

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { Route, BrowserRouter as Router, Routes } from "react-router";
 import { SignUp } from "./pages/SignUp";
 import { Login } from "./pages/Login";
 import { AuthLayout } from "./layout/AuthLayout";
-import { useAuth } from "./context/useAuth";
+import { Homepage } from "./pages/Homepage";
+import { ProtectedRoute } from "./pages/ProtectedRoute";
 
 function App() {
-	// const {isAuthenticated} = useAuth();
-
 	return (
 		<Router>
 			<Routes>
+				<Route path="/" element={<ProtectedRoute />}>
+					<Route index element={<Homepage />} />
+				</Route>
 				<Route path="/auth" element={<AuthLayout />}>
 					<Route path="/auth/signup" element={<SignUp />} />
 					<Route path="/auth/login" element={<Login />} />
